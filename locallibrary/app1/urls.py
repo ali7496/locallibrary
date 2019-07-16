028ac5861528ad6496ce28ef9db32d86cbb6321e
ali7496 <aliqamari2111@gmail.com>
2019-07-16 10:56:37 +0430

    add model

--- locallibrary/app1/urls.py
+++ locallibrary/app1/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
+from . import views
+app_name= 'app1'
 urlpatterns = [
 
+    url(r'^$', views.index, name='index'),
 ]
--- locallibrary/app1/views.py
+++ locallibrary/app1/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def index(request):
+    num_books= models.Book.objects.all().count()
+    num_instances= models.BookInstance.objects.all().count()
+    num_instances_available= models.BookInstance.objects.filter(status__exact='a').count()
+    num_authors= models.Author.objects.all().count()
+    return render(
+        request,
+        'app1/index.html',
+        context= {'num_books': num_books, 'num_instances': num_instances,'num_instances_available': num_instances_available, 'num_authors': num_authors}
+    )
